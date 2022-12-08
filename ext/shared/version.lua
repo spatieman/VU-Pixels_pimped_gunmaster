1c41b157423cbe994599594871f86a48ffa4f758
Ton Muller <spatieman@gmail.com>
2022-12-08 09:56:56 +0100

    Add files via upload

--- ext/shared/version.lua
+++ ext/shared/version.lua
@@ -1,4 +1,4 @@
-localModVersion = "1.0.5";
+localModVersion = "1.0.6";
 -- current version for mod.json
 -- do not forgot to update mod.json and version.lua if u sync with github !
 -- reason: upgrade - patch - bugfix - new release - stable release - test
