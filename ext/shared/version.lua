365b03afd20e3809b2f29dab56e1f4e541e4e835
Ton Muller <spatieman@gmail.com>
2022-12-10 14:25:01 +0100

    Delete ext/shared directory

--- ext/shared/version.lua
+++
@@ -1,4 +0,0 @@
-localModVersion = "1.0.8";
--- current version for mod.json
--- do not forgot to update mod.json and version.lua if u sync with github !
--- reason: upgrade - patch - bugfix - new release - stable release - test
